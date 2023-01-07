abc0fdc7bbb2689ba7af7dcd7b518ab06a023266
yuqiang6149 <yuqiang6149@163.com>
2023-01-07 15:55:44 +0800

    successfully load map by (rosrun perceptive_mpc load_map.sh)

--- perceptive_mpc/scripts/load_map.sh
+++ perceptive_mpc/scripts/load_map.sh
@@ -1,3 +1,3 @@
 #!/bin/bash          
 source devel/setup.bash
-rosservice call /voxblox_node/load_map "file_path: '/usr/src/perceptive_mpc_ws/src/perceptive_mpc/example/example_map.esdf'"
+rosservice call /voxblox_node/load_map "file_path: '/home/vision/yq_ws/perceptive_mpc_ws/src/perceptive_mpc/example/example_map.esdf'"
